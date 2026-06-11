b04404393081d5e69306e9287a246db32c2cc155
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-11 14:33:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -87,16 +87,3 @@ categoryButtons.forEach(button => {
 
   });
 
-});
-
-.category-list button.active{
-
-  background:#7c3aed;
-
-  color:white;
-
-  box-shadow:
-
-    0 0 20px rgba(124,58,237,.25);
-
-}
